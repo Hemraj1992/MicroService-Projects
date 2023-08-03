18b7efe00934076b0ad3a6ff8347eb24430cd4ef
Hemraj Kumawat <hhemraj.kumawat@gmail.com>
2023-08-03 17:32:17 +0530

    lall commit

--- Learn/src/hemraj/Java_9/Collections.java
+++ Learn/src/hemraj/Java_9/Collections.java
@@ -1,20 +1,36 @@
 package hemraj.Java_9;
 
+import java.time.Duration;
 import java.time.ZoneId;
 import java.util.stream.Stream;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class Collections {
 	public static void main(String[] args) {
-	      var names = List.of("Julie", "Robert", "Chris", "Joseph"); 
-	      for (var name : names) {
-	         System.out.println(name);
-	      }
-	      System.out.println("");
-	      for (var i = 0; i < names.size(); i++) {
-	         System.out.println(names.get(i));
+	      HttpClient httpClient = HttpClient.newBuilder()
+	         .version(HttpClient.Version.HTTP_2)
+	         .connectTimeout(Duration.ofSeconds(10))
+	         .build(); 
+	         try {
+	            HttpRequest request = HttpRequest.newBuilder()
+	            .GET()
+	            .uri(URI.create("https://www.google.com"))
+	            .build();                              
+	            HttpResponse<String> response = httpClient.send(request,
+	            HttpResponse.BodyHandlers.ofString()); 
+
+	         System.out.println("Status code: " + response.statusCode());                            
+	         System.out.println("Headers: " + response.headers().allValues("content-type"));
+	         System.out.println("Body: " + response.body());
+	      } catch (IOException | InterruptedException e) {
+	         e.printStackTrace();
 	      }
 	   }
+
 }
