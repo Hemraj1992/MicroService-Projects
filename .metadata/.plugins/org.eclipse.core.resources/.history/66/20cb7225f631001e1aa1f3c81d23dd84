974637ca134be8395b9eacc825950c925d2142bf
Hemraj Kumawat <hhemraj.kumawat@gmail.com>
2023-08-03 17:43:31 +0530

    two DB config removed

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/66/20cb7225f631001e1aa1f3c81d23dd84
@@ -0,0 +1,52 @@
+package com.HotelStaff.Entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Staff {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer Id;
+
+	private String name;
+
+	private String mobile;
+
+	private String city;
+
+	public Integer getId() {
+		return Id;
+	}
+
+	public void setId(Integer id) {
+		Id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+}
--- HotelService/src/main/java/com/HotelService/Config/ServiceDBConfig.java
+++ HotelService/src/main/java/com/HotelService/Config/ServiceDBConfig.java
@@ -1,77 +1,77 @@
-package com.HotelService.Config;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import javax.sql.DataSource;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.core.env.Environment;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.JpaVendorAdapter;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-@Configuration
-@EnableTransactionManagement
-@EnableJpaRepositories(
-		entityManagerFactoryRef = "serviceDataSource",
-		basePackages = {"com.HotelService.Repository"},
-		transactionManagerRef = "serviceTransactionManager"
-		)
-public class ServiceDBConfig {
-	
-	@Autowired
-	private Environment environment;
-	
-	@Bean("serviceDataSource")
-	@Primary
-	public DataSource dataSource() {
-		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		
-		dataSource.setUrl(environment.getProperty("spring.datasource.url"));
-		dataSource.setUsername(environment.getProperty("spring.datasource.username"));
-		dataSource.setPassword(environment.getProperty("spring.datasource.password"));
-		
-		return dataSource;
-		
-	}
-	
-	@Bean(name = "serviceEntityManagerFactoryBean")
-	public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean() {
-		
-		LocalContainerEntityManagerFactoryBean bean = new LocalContainerEntityManagerFactoryBean();
-		
-		bean.setDataSource(dataSource());
-		
-		Map<String, String> jpaProperties = new HashMap<String, String>();
-		jpaProperties.put("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
-		jpaProperties.put("hibernate.show_sql", "true");
-		jpaProperties.put("hibernate.hbm2ddl.auto", "update");
-		bean.setJpaPropertyMap(jpaProperties);
-		
-		JpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
-		bean.setJpaVendorAdapter(adapter);
-		
-		bean.setPackagesToScan("com.HotelService.Entity");
-		
-		return bean;
-		
-	}
-	
-	@Bean(name = "serviceTransactionManager")
-	@Primary
-	public PlatformTransactionManager transactionManager() {
-		JpaTransactionManager transManager = new JpaTransactionManager();
-		transManager.setEntityManagerFactory(entityManagerFactoryBean().getObject());
-		return transManager;
-	}
-
-}
+//package com.HotelService.Config;
+//
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//import javax.sql.DataSource;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Primary;
+//import org.springframework.core.env.Environment;
+//import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+//import org.springframework.jdbc.datasource.DriverManagerDataSource;
+//import org.springframework.orm.jpa.JpaTransactionManager;
+//import org.springframework.orm.jpa.JpaVendorAdapter;
+//import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+//import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+//import org.springframework.transaction.PlatformTransactionManager;
+//import org.springframework.transaction.annotation.EnableTransactionManagement;
+//
+//@Configuration
+//@EnableTransactionManagement
+//@EnableJpaRepositories(
+//		entityManagerFactoryRef = "serviceDataSource",
+//		basePackages = {"com.HotelService.Repository"},
+//		transactionManagerRef = "serviceTransactionManager"
+//		)
+//public class ServiceDBConfig {
+//	
+//	@Autowired
+//	private Environment environment;
+//	
+//	@Bean("serviceDataSource")
+//	@Primary
+//	public DataSource dataSource() {
+//		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//		
+//		dataSource.setUrl(environment.getProperty("spring.datasource.url"));
+//		dataSource.setUsername(environment.getProperty("spring.datasource.username"));
+//		dataSource.setPassword(environment.getProperty("spring.datasource.password"));
+//		
+//		return dataSource;
+//		
+//	}
+//	
+//	@Bean(name = "serviceEntityManagerFactoryBean")
+//	public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean() {
+//		
+//		LocalContainerEntityManagerFactoryBean bean = new LocalContainerEntityManagerFactoryBean();
+//		
+//		bean.setDataSource(dataSource());
+//		
+//		Map<String, String> jpaProperties = new HashMap<String, String>();
+//		jpaProperties.put("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
+//		jpaProperties.put("hibernate.show_sql", "true");
+//		jpaProperties.put("hibernate.hbm2ddl.auto", "update");
+//		bean.setJpaPropertyMap(jpaProperties);
+//		
+//		JpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+//		bean.setJpaVendorAdapter(adapter);
+//		
+//		bean.setPackagesToScan("com.HotelService.Entity");
+//		
+//		return bean;
+//		
+//	}
+//	
+//	@Bean(name = "serviceTransactionManager")
+//	@Primary
+//	public PlatformTransactionManager transactionManager() {
+//		JpaTransactionManager transManager = new JpaTransactionManager();
+//		transManager.setEntityManagerFactory(entityManagerFactoryBean().getObject());
+//		return transManager;
+//	}
+//
+//}
